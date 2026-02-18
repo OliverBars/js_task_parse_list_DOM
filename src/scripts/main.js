4997ef9e423ebe46810987f267377d01ae3e02ad
Yaroslav Grom <gromyarik3@gmail.com>
2026-02-18 12:53:40 +0100

    check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelector('ul');
+const items = [...document.querySelectorAll('li')];
+
+items.sort((a, b) => {
+  const getNumericSalary = (el) => {
+    return parseFloat(el.dataset.salary.replace(/[^0-9.-]+/g, ''));
+  };
+
+  return getNumericSalary(a) - getNumericSalary(b);
+});
+
+items.forEach((item) => list.appendChild(item));
